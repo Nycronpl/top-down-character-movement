be022201327166161a455069c26a0470bc6e245d
Nycronpl <nycronpl@gmail.com>
2026-09-19 12:45:57 +0200

    Change how movement is being applied
    Movement is now being applied using rigidbody instead of transform. This change eliminates jittering when player tries to push against the solid wall.

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -2,15 +2,18 @@ using UnityEngine;
 
 namespace Nycron.TopDown.Movement
 {
+    [RequireComponent(typeof(Rigidbody2D))]
     public class PlayerMovement : MonoBehaviour
     {
         [SerializeField] private float speedMultiplier = 1f;
 
         private MovementInput input;
+        private Rigidbody2D body;
 
         private void Awake()
         {
             input = new MovementInput();
+            body = GetComponent<Rigidbody2D>();
         }
 
         private void OnEnable()
@@ -25,8 +28,9 @@ namespace Nycron.TopDown.Movement
 
         private void Update()
         {
+            //doesn't use tranform.position because it creates jittering when pushing against the wall
             Vector2 move = input.Movement.Move.ReadValue<Vector2>();
-            transform.position += (Vector3)move.normalized * speedMultiplier * Time.deltaTime;
+            body.linearVelocity = move * speedMultiplier;
         }
     }
 }
