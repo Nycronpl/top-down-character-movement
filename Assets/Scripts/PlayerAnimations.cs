55f0bc3187241f3e3fbeddeaf13d6a4d921afacd
Nycronpl <nycronpl@gmail.com>
2026-09-20 11:38:41 +0200

    Connect animations to the player

--- Assets/Scripts/PlayerAnimations.cs
+++ Assets/Scripts/PlayerAnimations.cs
@@ -6,6 +6,22 @@ namespace Nycron.TopDown.Movement
     [RequireComponent(typeof(PlayerMovement))]
     public class PlayerAnimations : MonoBehaviour
     {
+        private Animator animator;
+        private PlayerMovement playerMovement;
 
+        private void Awake()
+        {
+            animator = GetComponent<Animator>();
+            playerMovement = GetComponent<PlayerMovement>();
+        }
+
+        private void Update()
+        {
+            bool isPlayerMoving = playerMovement.IsPlayerMoving();
+            animator.SetBool("Moving", isPlayerMoving);
+
+            if (isPlayerMoving)
+                animator.SetInteger("Direction", (int)playerMovement.GetPlayerDirection());
+        }
     }
 }
